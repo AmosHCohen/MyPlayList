711e9ae3d569c1cf82faf0f486026316a6ea92a6
AmosCohen <amoshirac@gmail.com>
2022-07-20 08:58:15 +0300

    add header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,11 +2,22 @@ import './Header.css'
 import { Link } from 'react-router-dom';
 export default function Header() {
     return (
-        <div id="header">
-            <span> <Link to="/">Home</Link></span>
-            <span> <Link to="/login">Login</Link></span>
-            <span> <Link to="/search">Search</Link></span>
-            <span> <Link to="/logout">Logout</Link></span>
+        <div id="header" className='headerContainer'>
+            <div className='headerInnerWidth'>
+                <div className='workshopHeader'>
+                    <div> <Link to="/">Home</Link></div>
+                    <div> <Link to="/login">Login</Link></div>
+                    <div> <Link to="/search">Search</Link></div>
+                    <div> <Link to="/logout">Logout</Link></div>
+                </div>
+                <div>Our playList</div>
+
+                {/* <img
+                    src={threeLines}
+                    alt="threeLines"
+                    className='threeLines'
+                /> */}
+            </div>
         </div>
     )
 }
